2dc9b04dd6d21716378b212a2bc50415dd1cb37f
anngiepp <angie.portocarrero@tecsup.edu.pe>
2026-06-06 22:36:03 -0500

    feat: integrar consumo de API de Open Trivia DB

--- src/services/triviaService.js
+++ src/services/triviaService.js
@@ -5,4 +5,4 @@ const API_URL = "https://opentdb.com/api.php?amount=10";
 export async function getTriviaQuestions() {
   const response = await axios.get(API_URL);
   return response.data.results;
-}
+} 
